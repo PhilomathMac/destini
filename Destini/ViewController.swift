922732c723adb70a4693b355335f0b62af03fbac
McKenzie Macdonald <kenziemacd@icloud.com>
2022-07-28 11:45:50 -0700

    Initial Commit

---
+++ Destini/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Destini
+//
+//  Created by McKenzie Macdonald on 7/28/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
