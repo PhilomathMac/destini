b9ff853da8a135b0beff9574adeb70891af8cd51
McKenzie Macdonald <kenziemacd@icloud.com>
2022-07-28 12:27:14 -0700

    Add hardcoded data to viewcontroller

--- Destini/Controller/ViewController.swift
+++ Destini/Controller/ViewController.swift
@@ -12,9 +12,17 @@ class ViewController: UIViewController {
     @IBOutlet var choice1Button: UIButton!
     @IBOutlet var choice2Button: UIButton!
     
+    let story0 = "You see a fork in the road."
+    let choice1 = "Take a left."
+    let choice2 = "Take a right."
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        storyLabel.text = story0
+        choice1Button.setTitle(choice1, for: .normal)
+        choice2Button.setTitle(choice2, for: .normal)
     }
 
     @IBAction func choiceButtonClicked(_ sender: UIButton) {
